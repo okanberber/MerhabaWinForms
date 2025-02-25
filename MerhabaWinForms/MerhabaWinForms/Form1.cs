55e46d986a73775aab41ba15026c8dd6ae8cdba5
Okan Berber <okan.berber@outlook.com.tr>
2025-02-25 23:37:16 +0300

    Hello WinForms

--- MerhabaWinForms/MerhabaWinForms/Form1.cs
+++ MerhabaWinForms/MerhabaWinForms/Form1.cs
@@ -14,7 +14,12 @@ namespace MerhabaWinForms
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); // Bu metot sürükle bırak ile aşağıdaki yazmış olduğumuz kodları zaten otomatik bir şekilde yazıyor.
+            Label lbl_murtaza = new Label();
+            lbl_murtaza.Text = "Ben Murtaza Nasılsınız ?";
+            lbl_murtaza.AutoSize = true;
+            lbl_murtaza.Location = new Point(40, 100); // Point de bir nesnedir. bizden x,y koordinatlarını ister. Bu sayede label için lokasyon belirtebiliriz.
+            this.Controls.Add(lbl_murtaza); // Ram üzerinde var edildi ancak henüz forma eklenmedi. Forma eklemek için bu kodu yazıyoruz. Form eklenenleri list olarak aldığından add komutu ile ekliyoruz.
         }
     }
 }
